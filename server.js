5723d4a6097399e7d5078182b09723f2ccbe9b2b
Jennifer Nava <jennifernava681@gmail.com>
2025-03-12 15:40:38 -0500

    g

---
+++ server.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log("Client ID:", process.env.def5390a-6467-4999-87f2-947e87add8d9);
+console.log("Tenant ID:", process.env.170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259);
+console.log("Subscription ID:",process.env.13f10d78-4ea3-4bb4-ad51-dd49fb86fbdc);
